8addeb4179745b011b852a8d116c64d3ffba5979
ymmmtym <h.yukihisa.378@gmail.com>
2019-09-29 20:53:03 +0900

    create base page

---
+++ app/src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="mb-0 mt-5">
+    <nav class="navbar n avbar-dark fixed-top">
+      <ul>
+        <li>top</li>
+        <li>このページについて</li>
+        <li>portfolio</li>
+        <li>skill</li>
+        <li>contact</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+nav {
+  background-color: rgba(65, 105, 255, 0.3);
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ app/src/components/Main.vue
@@ -0,0 +1,39 @@
+<template>
+  <main class="container">
+    <h1 class="my-3 py-3">Welcome to ymmmtym's portfolio</h1>
+    <img src="http://www.wanpug.com/illust/illust3470.png" alt="工事中">
+    <h2>ただいま、サイト製作中です。</h2>
+    <p>
+      お越し頂きありがとうございます。<br>
+      ただいまホームページを作成中です。<br>
+      申し訳ございませんが、今しばらくお待ちください。
+    </p>
+
+    <h1>portfolio</h1>
+    <a href="https://keywoo.herokuapp.com" target="_blank">keywoo</a>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+img{
+  width: 100%;
+}
+</style>
